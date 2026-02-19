a042c8a2a71c96da59646b80455ad74a98b3c682
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-02-19 18:17:40 +1100

    fix: warnings for single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,7 @@ pub fn run() {
 
     #[cfg(desktop)]
     {
-        builder = builder.plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let _ = app
                 .get_webview_window("main")
                 .expect("no main window")
